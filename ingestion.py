c2c8230dd6db67af90fb443a85657cc1931adde1
AndriiSyniuchenko <andriisyniuchenko@gmail.com>
2026-05-06 12:30:16 -0700

    Add ingestion pipeline

---
+++ ingestion.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_groq import ChatGroq
+from langchain_ollama import OllamaEmbeddings
+
+
+load_dotenv()
+
+EMBED_MODEL = os.getenv("EMBED_MODEL")
+
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=250, chunk_overlap=0)
+
+
+doc_splits = text_splitter.split_documents(docs_list)
+
+"""vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    embedding=OllamaEmbeddings(model=EMBED_MODEL),
+    persist_directory="./.chroma",
+)"""
+
+#retriever = vectorstore.as_retriever()
+
+
+retriever = Chroma(
+    collection_name="rag-chroma",
+    persist_directory="./.chroma",
+    embedding_function=OllamaEmbeddings(model=EMBED_MODEL),
+).as_retriever()
